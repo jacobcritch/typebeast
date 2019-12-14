cdbfe0d293d672e730be2a56d4034bb7aa135ab4
Jacob Critch <jacobc@mun.ca>
2019-12-14 17:42:47 -0330

    Added typing test functionality

---
+++ main.js
@@ -0,0 +1,36 @@
+Reset();
+function Reset(){
+	wordCount = 0;
+	playing = false;
+
+	gameText = 'The quick brown fox jumped over the long winding fence';
+	document.getElementById('gametext').innerHTML = gameText;
+	gameTextArrOrg = gameText.split(" ");
+	gameTextArr = gameText.split(" ");
+	gameTextArrLength = gameTextArr.length;
+	document.getElementById('gameinput').disabled = false;
+	document.getElementById('wpm').innerHTML = 'WPM: ?'
+}
+
+function GameLoop()
+{	
+	if (playing === false){playing = true; startTime = new Date();}
+	inputElement = document.getElementById('gameinput');
+	input = inputElement.value;
+	
+	if ( (input === gameTextArr[0] + " ") || (input === gameTextArrOrg[gameTextArrLength-1] && wordCount===gameTextArrLength-1) ) {
+		wordCount += 1;
+		gameTextArr.shift();
+		document.getElementById('gametext').innerHTML = gameTextArr.join(' ')
+		document.getElementById('gameinput').value = ""
+		if (wordCount === gameTextArrLength){
+			endTime = new Date();
+			document.getElementById('gameinput').disabled = true;
+			WPM = (gameText.length / 5) / (((endTime-startTime) / 1000) / 60);
+			WPM = WPM.toString();
+			WPM = WPM.substr(0,5);
+			document.getElementById('wpm').innerHTML = 'WPM: ' + WPM;
+			wordCount = 0;
+		}
+	}
+}
